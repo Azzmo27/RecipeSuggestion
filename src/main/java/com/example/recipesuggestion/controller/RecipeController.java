ba1ee1ab91460a89cf3d6f6410d91eab39a741e4
Azra <Azmo0001@stud.kea.dk>
2024-11-18 00:15:06 +0100

    Done

--- src/main/java/com/example/recipesuggestion/controller/RecipeController.java
+++ src/main/java/com/example/recipesuggestion/controller/RecipeController.java
@@ -1,4 +1,5 @@
 package com.example.recipesuggestion.controller;
+
 import com.example.recipesuggestion.dto.ChatRequest;
 import com.example.recipesuggestion.dto.ChatResponse;
 import com.example.recipesuggestion.dto.Choice;
@@ -16,8 +17,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-
 @RestController
 public class RecipeController {
     @Value("${OPENAI_API_KEY}")
@@ -27,40 +26,6 @@ public class RecipeController {
 
     public RecipeController(WebClient.Builder webClientBuilder) {
         this.webClient = webClientBuilder.baseUrl("https://api.openai.com/v1/chat/completions").build();
-    }
-
-    @GetMapping("/chat")
-    public Map<String, Object> chatWithGPT(@RequestParam String message) {
-        ChatRequest chatRequest = new ChatRequest();
-        chatRequest.setModel("gpt-3.5-turbo");
-
-        List<Message> lstMessages = new ArrayList<>();
-        lstMessages.add(new Message("system", "You are a helpful assistant."));
-        lstMessages.add(new Message("user", "Where is " + message));
-
-        chatRequest.setMessages(lstMessages);
-        chatRequest.setN(3);
-        chatRequest.setTemperature(2.1);
-        chatRequest.setMaxTokens(30);
-        chatRequest.setStream(false);
-        chatRequest.setPresencePenalty(1.2);
-
-        ChatResponse response = webClient.post()
-                .contentType(MediaType.APPLICATION_JSON)
-                .headers(h -> h.setBearerAuth(openapikey))
-                .bodyValue(chatRequest)
-                .retrieve()
-                .bodyToMono(ChatResponse.class)
-                .block();
-
-        List<Choice> lst = response.getChoices();
-        Usage usg = response.getUsage();
-
-        Map<String, Object> map = new HashMap<>();
-        map.put("Usage", usg);
-        map.put("Choices", lst);
-
-        return map;
     }
 
     @GetMapping("/recipeSuggestion")
@@ -79,6 +44,7 @@ public class RecipeController {
         chatRequest.setStream(false);
         chatRequest.setPresencePenalty(0.5);
 
+
         ChatResponse response = webClient.post()
                 .contentType(MediaType.APPLICATION_JSON)
                 .headers(h -> h.setBearerAuth(openapikey))
@@ -89,6 +55,7 @@ public class RecipeController {
 
         List<Choice> lst = response.getChoices();
         Usage usg = response.getUsage();
+
 
         Map<String, Object> map = new HashMap<>();
         map.put("Usage", usg);
